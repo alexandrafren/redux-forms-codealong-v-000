e9c337c93e9d2c00f1c91174a3c8432ca6f86036
Alexandra Fren <alexandra.fren@gmail.com>
2019-03-29 14:37:22 +0000

    Automatically backed up by Learn

--- src/components/todos/CreateTodo.js
+++ src/components/todos/CreateTodo.js
@@ -34,7 +34,7 @@ class CreateTodo extends Component {
 
 const mapDispatchToProps = dispatch => {
   return {
-    addTodo: () => dispatch(<some action>)
+    addTodo: () => dispath({ type: 'ADD_TODO', payload: formData ))
   }
 }
 
